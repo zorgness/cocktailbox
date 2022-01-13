3970563ae2d3c622022954b2e7d132b8d4dfb374
zorgness <tfouquert@gmail.com>
2022-01-13 10:16:35 +0100

    first commit

--- final/settings.py
+++ final/settings.py
@@ -146,7 +146,7 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
