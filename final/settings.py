b9efca4431d97bf154e1bf002ebf36c6ef074ccf
zorgness <tfouquert@gmail.com>
2022-01-13 10:40:15 +0100

    update setting

--- final/settings.py
+++ final/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = get_env_variable('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'thecocktailbox.herokuapp.com']
 
 
 # Application definition
